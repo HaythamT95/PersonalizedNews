eaa4bb6658117a3f64e163ffcfc3e6a6a8457437
HaythamT95 <haythamtaweel95@gmail.com>
2024-07-08 11:34:19 +0300

    updated service and accessor

--- accessor/controller/newsAccessorContorller.js
+++ accessor/controller/newsAccessorContorller.js
@@ -1,29 +1,29 @@
 import express from "express";
-import axios from "axios";
 import dotenv from "dotenv"
-import getCurrentDateMinusTwoDays from "../service/newsService.js";
+import { getCurrentDateMinusTwoDays, fetchNews } from "../service/newsService.js";
 
 dotenv.config();
 const newsController = express.Router();
 
 newsController.get('/api/news', async (req, res) => {
-    const { category } = req.body;
+    const { preferences } = req.body;
     const earliestPublishedDate = getCurrentDateMinusTwoDays();
 
-    try {
-        const response = await axios.get("https://api.apilayer.com/world_news/search-news", {
-            headers: {
-                'apikey': process.env.NEWS_API,
-            },
-            params: {
-                text: category ? category : '',
-                language: 'en',
-                'earliest-publish-date': earliestPublishedDate,
-                number: '2'
-            },
-        });
+    let news = [];
 
-        res.status(200).json({ data: response.data })
+    try {
+        for (const category of preferences.newsCategories) {
+            const response = await fetchNews(category, earliestPublishedDate);
+            console.log(response)
+        }
+
+        for (const category of preferences.techUpdates) {
+            const response = await fetchNews(category, earliestPublishedDate);
+            console.log(response)
+
+        }
+
+        res.status(200).json({ data: "success" })
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
--- accessor/service/newsService.js
+++ accessor/service/newsService.js
@@ -1,14 +1,33 @@
+import axios from "axios";
+import dotenv from "dotenv"
+dotenv.config();
+
 const getCurrentDateMinusTwoDays = () => {
     const currentDate = new Date();
-    
+
     currentDate.setDate(currentDate.getDate() - 2);
-    
+
     const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
     const day = String(currentDate.getDate()).padStart(2, '0');
-    
+
     const formattedDate = `${year}-${month}-${day} 00:00:00`;
     return formattedDate;
 };
 
-export default getCurrentDateMinusTwoDays;
+const fetchNews = async (category, earliestPublishedDate) => {
+    const response = await axios.get("https://api.apilayer.com/world_news/search-news", {
+        headers: {
+            'apikey': process.env.NEWS_API,
+        },
+        params: {
+            text: category ? category : '',
+            language: 'en',
+            'earliest-publish-date': earliestPublishedDate,
+            number: '2'
+        },
+    });
+    return response.data;
+}
+
+export { getCurrentDateMinusTwoDays, fetchNews };
